d5241c805d96ee4af96b5fd2835d85dcaf4b0329
Zinhle <zinhle.nhlapo31@gmail.com>
2024-08-23 10:04:54 +0200

    Created a recipe details page and created an array of all our recipes that we have including their ingredients and instructions. I also added a button to return to the previous page.

---
+++ recipe-book/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RecipeDetails from "./components/RecipeDetails";
+
+const App = () => {
+  return (
+    <div>
+      <RecipeDetails/>
+    </div>
+  );
+}
+
+export default App;
